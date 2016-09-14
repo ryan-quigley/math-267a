69ef4c8aeeb4f0532ec9616770b632661bbfa8bd
Ryan Quigley <ryan-quigley@users.noreply.github.com>
2016-09-13 19:01:31 -0700

    Final versions (pending else-if confirmation)

--- Sec2GroupGP1.R
+++ Sec2GroupGP1.R
@@ -2,11 +2,11 @@ solveQuadr <- function(a = 1, b, c) {
 	# d: discriminant
 	d = b^2 - 4*a*c
 	
-	# check sign of discriminant
+	# Check sign of discriminant
 	if (d < 0) {
 		cat("There are no real roots.")
 		invisible(0)
-	} else if (d > 0){
+	} else if (d > 0) {
 		r1 = (-b - d^(0.5))/(2*a)
 		r2 = (-b + d^(0.5))/(2*a)
 		cat("1st root: ", r1, "\n", "2nd root: ", r2, sep = "")
@@ -17,12 +17,3 @@ solveQuadr <- function(a = 1, b, c) {
 		invisible(1)
 	}
 }
-
-# DO NOT INCLUDE IN FINAL ANSWER
-# Code for testing the return value and print output
-x <- solveQuadr(1, -3, 4)
-x
-y <- solveQuadr(-4, 12, -9)
-y
-z <- solveQuadr(2, -1, -8)
-z
--- Sec2GroupGP2.R
+++ Sec2GroupGP2.R
@@ -1,38 +1,21 @@
 getDigit <- function(x, n) {
-	# check if x is real
+	# According to the is.numeric help page: is.numeric returns TRUE if its argument is of mode "numeric" (type "double" or type "integer")
+	
+	# Check if x is real
 	if (!is.numeric(x)) {
-		stop("x has to be a real number.", call. = TRUE)
-	} else if (((n %% 1) != 0) || (n < 0)) {
-		# note: I am not using the function is.integer because R stores "integers" such as 1, 2, 3,... as numeric by default
-		# thus the condition would fail unless the user specifcally casts the value as an integer before or during the function call
-		stop("n has to be a positive integer.", call. = TRUE)
+		stop("x has to be a real number.")
+	}
+	
+	# Check if n is a positive integer
+	if ((!is.numeric(n)) || (n %% 1 != 0) || (n < 0)) {
+		stop("n has to be a positive integer.")
+	}
+	
+	# Check if x is negative; if so, make positive because the arithmetic is easier
+	if (x < 0) {
+		y <- -x
+		((y * 10 ** n) %/% 1) %% 10
 	} else {
-		((x*(10^n)) %/% 1) - ((x*(10^(n-1))) %/% 1)*10
-	}	
+		((x * 10 ** n) %/% 1) %% 10
+	}
 }
-
-
-# DO NOT INCLUDE IN FINAL SOLUTION
-# Demonstrating that even though 1 is an integer, R stores it as a numeric
-x <- 1
-if (is.integer(x)) print(x)
-
-# Conditions that need to be test
-# 1) x is a real number => try entering a complex number of a logical
-# 2) n must be an integer
-# 3) n must be positive
-
-# Positive test cases
-for (i in 1:16) {
-	gd <- getDigit(1.1234567890123456, n = i)
-	print(gd)
-}
-getDigit(1, n = 2)
-getDigit(20, n = 2)
-getDigit(300, n = 2)
-
-# Negative test cases
-getDigit(1.234, n = -1)
-getDigit(1.234, n = 1.5)
-getDigit(TRUE, n = 1)
-getDigit(1 + 2i, n = 1)

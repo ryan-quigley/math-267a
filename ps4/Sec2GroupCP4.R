3eb98bcea9d6e5f60bd71591852239db9cf5a803
Ryan Quigley <rquig14@gmail.com>
2016-09-27 16:29:48 -0700

    Correction to problem 4

--- ps4/Sec2GroupCP4.R
+++ ps4/Sec2GroupCP4.R
@@ -11,16 +11,17 @@ space.rm <- gsub('\\s', '.', name)
 user <- gsub('([A-Z])', '\\L\\1', space.rm, perl = TRUE)
 email <- paste(user, "@ponyville.edu", sep = "")
 
-user2 <- gsub('([A-Z][a-z]*)\\s?([A-Z]?[a-z]*)', '\\L\\1.\\L\\2', name, perl = TRUE)  # not working: appends period to single name entries
 
 # b)
-groups <- c("A", "B", "C")
+groups <- LETTERS[1:(length(name)/3)]
 rs <- sample(email, size = length(email))
 name.ordered <- name[match(rs, email)]
-ps1grps <- data.frame(name = name.ordered, email = rs, ps1 = groups)  # should the groups be a factor???
+ps1grps <- data.frame(name = name.ordered, email = rs, ps1 = groups)
+
 
 # c)
 write.table(ps1grps, "ps1grps.txt", quote = FALSE, sep = ",", row.names = FALSE)
+
 
 # d)
 ps1grps.in <- read.table("ps1grps.txt", header = TRUE, sep = ",")

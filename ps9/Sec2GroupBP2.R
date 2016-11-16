321b439635c64b1cca1a78f0b839b67199032802
Ryan Quigley <rquig14@gmail.com>
2016-11-16 07:34:53 -0800

    Code for problems 2 and 3

---
+++ ps9/Sec2GroupBP2.R
@@ -0,0 +1,33 @@
+# MATH 267A
+# Section 2
+# Group D
+#  Beatriz Hernandez
+#  Jimmy Nguyen
+#  Ryan Quigley
+# Problem 2
+
+
+x <- c(-1,0,0,1,1,2,2,3,3,4)
+y <- c(0,0,0,1/4,1/2,1/2,2/3,3/4,1,1)
+open <- c(0,0,0,1,0,1,0,1,0,0)
+closed <- c(0,0,0,0,1,0,1,0,1,0)
+df <- data.frame(x, y, open, closed)
+
+plot(df$x, df$y, type = "n", axes = FALSE, ann = FALSE)
+df <- data.frame(x, y, open, closed)
+n <- dim(df)[1] - 1
+
+for (i in 1:n) {
+	if (df$x[i] != df$x[i+1]) {
+		lines(df[c(i, i+1), 1:2], col = "violetred")
+	} else {
+		lines(df[c(i, i+1), 1:2], lty = 3, col = "grey50")
+	}
+}
+points(df[df$closed == 1, 1:2], pch = 20)
+points(df[df$open == 1, 1:2], pch = 21, col = "black", bg = "white", cex = (2/3))
+
+axis(1, at = seq.int(-1,4), col = "grey50", lwd = 0, lwd.tick = 1)
+box(lty = 1, col = "grey50")
+axis(2, at = seq.int(0, 1, 0.2), lwd = 0, lwd.tick = 1, col = "grey50")
+title(main = "CDF of Random Variable X", xlab = "x", ylab = "F(x)")
---
+++ ps9/Sec2GroupBp3.R
@@ -0,0 +1,44 @@
+# MATH 267A
+# Section 2
+# Group D
+#  Beatriz Hernandez
+#  Jimmy Nguyen
+#  Ryan Quigley
+# Problem 3
+
+
+load("ps9p3.RData")
+summary(x)
+m <- mean(x)
+s <- sd(x)
+
+
+#hist(x, breaks = 20, freq = FALSE, right = FALSE, col = "royalblue", border = "aquamarine", xlim = c(0,1))
+
+
+h <- 0.05
+krn <- "gaussian"
+d <- density(x, kernel = krn, bw = h, from = -0.5, to = 1.5)
+
+y.up <- ceiling(max(d$y)) + 1
+plot(d, axes = FALSE, ann = FALSE, ylim = c(0, y.up), col = "violetred")
+axis(1, at = seq.int(-0.5, 1.5, 0.5), col = "grey50")
+axis(2, at = seq.int(0, y.up, 0.5), col = "grey50")
+
+curve(dnorm(x, mean = m, sd = s), from = -0.5, to = 1.5, col = "slateblue2", lty = 2, add = TRUE)
+curve(dbeta(x, shape1 = 7.5, shape2 = 6.5), from = 0, to = 1, col = "black", lty = 3, add = TRUE)
+
+legend("topright", inset = 0.05, 
+	legend = c(paste(krn, "(h = ", h, ")", sep = ""), 
+		paste("N(", round(m,3),", ",round(s^2,3),")", sep = ""),
+		paste("Beta(7.5, 6.5)")
+	), 
+	bty = "n", col = c("violetred", "slateblue2", "black"), 
+	lty = c(1,2,3), 
+	lwd = 1)
+title(xlab = "x", ylab = "Density")
+
+
+# x2 <- rnorm(length(x)*1000, mean = m, sd = s)
+# d2 <- density(x2, kernel = krn, bw = h, from = -0.5, to = 1.5)
+# lines(d2)

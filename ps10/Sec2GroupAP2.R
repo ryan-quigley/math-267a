16e635b054fada1fc62dcf0068fc107815dcceaa
Ryan Quigley <rquig14@gmail.com>
2016-11-27 17:56:07 -0800

    Code cleanup and final write up

--- ps10/Sec2GroupAP2.R
+++ ps10/Sec2GroupAP2.R
@@ -16,9 +16,9 @@ ciExp <- function(x, alpha) {
 		stop("alpha must be in the interval (0, 1)")
 	}
 	if (any(is.na(x))) {
-		warning("NAs removed from x, sample size reduced.")
+		warning(paste(sum(is.na(x)), "NAs removed from x, sample size reduced."))
 	}
-	n <- length(a[!is.na(a)])
+	n <- length(x[!is.na(x)])
 	lambda.hat <- 1/mean(x, na.rm = TRUE)
 	chi.sq.q <- qchisq(p = c(1 - alpha/2, alpha/2), df = 2*n)
 	ci <- round((2*n)/(lambda.hat*chi.sq.q), 2)
@@ -26,12 +26,10 @@ ciExp <- function(x, alpha) {
 	ci
 }
 
-#a <- c(7.44, 7.69, 0.96, 0.27, 2.03, NA, 1.38)
-#ciExp(a, alpha = 0.05)
+# Function validation:
+# a <- c(7.44, 7.69, 0.96, 0.27, 2.03, NA, 1.38)
+# ciExp(a, alpha = 0.05)
 
 # b)
-arr.time <- c(28.62, 2.14, 8.14, 2.17, 5.72, 10.64, 6.89, 32.09, 19.37, 3.07)
-arrival.ci <- ciExp(arr.time, alpha = 0.02)
-
-# c) In order to guarantee that the confidence interval contains the true value for the average time between events, the interval must be the entire support of the distribution: the support of the $\chi^2$ distribution is [0, \infty)
-ciExp(arr.time, 0)
+arv.time <- c(28.62, 2.14, 8.14, 2.17, 5.72, 10.64, 6.89, 32.09, 19.37, 3.07)
+arrival.ci <- ciExp(arv.time, alpha = 0.02)
--- ps10/Sec2GroupAP3.R
+++ ps10/Sec2GroupAP3.R
@@ -10,25 +10,13 @@
 load("ps10p3.RData")
 n <- length(trains)
 s.var <- var(trains)
+s.sd <- sd(trains)
 
-# Symbols
-# $H_0: \mu <= 140$
-# $H_a: \mu > 140$
-# Words
-# The null hypothesis: the previous survey results are still accurate and relevant
-# The alternative hypothesis: the old survey results are outdated and logging cars are actually being loaded more heavily in recent years.
-# One-sided test: we want to determine whether or not to build the trestle to higher tolerances to accommodate heavier cars, so we are really only interested in the case where the average weight is greater than the hypothesized value.
-# Rejection region: in this context type II error is a very serious problem. If we fail to reject the null hypothesis when in reality the average weight is greater than 140, then the engineering corps will not build the trestle to higher tolerances and the trestle will be at greater risk of structural failure. We can control type II error by making the rejection region larger, which can be achieved by choosing a lower bound for the region that is very close to 140. Determining that particular value should be left to the engineers, but for the sake of proceeding with the test we calculate it as follows. Say at a value of 150 for the true average weight we want to make sure the type II error is at most 1%:
-
-x.bar.c <- 150 + qt(0.01, df = 19)*sqrt(s.var/n)
+# critical values
+x.bar.c <- 145 + qt(0.01, df = 19)*sqrt(s.var/n)
 t.q.c <- (x.bar.c - 140)/sqrt(s.var/n)
 alpha.c <- pt(t.q.c, df = n - 1, lower.tail = FALSE)
-
 
 # b)
 t.test(trains, mu = 140, alternative = "greater")
 # Conclusion: reject the null hypothesis
-
-# c) the t-test from part (b) only allows us to advise the engineers to build the trestle to higher tolerance, it does not allow us to suggest a minimum threshold. [We would need a confidence interval for this]
-
-# d)
